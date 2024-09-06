f792287a559a2457b3c34f7068811b0a7346db69
Dana Maxfield <danam@synopsys.com>
2024-09-06 10:02:35 -0400

    IALERT-3724 - Update copyright

--- src/test/groovy/com/synopsys/integration/utility/BuildFileUtilityTest.groovy
+++ src/test/groovy/com/synopsys/integration/utility/BuildFileUtilityTest.groovy
@@ -1,3 +1,10 @@
+/*
+ * common-gradle-plugin
+ *
+ * Copyright (c) 2024 Black Duck Software, Inc.
+ *
+ * Use subject to the terms and conditions of the Black Duck Software End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.utility
 
 import org.junit.jupiter.api.Test
